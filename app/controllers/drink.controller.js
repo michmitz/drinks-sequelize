61eaefacee7c65dc81dd1b35a5ccf10b5a58e279
Michelle Stermitz <stermitzm@gmail.com>
2020-12-30 21:23:54 -0800

    Added CRUD functions

--- app/controllers/drink.controller.js
+++ app/controllers/drink.controller.js
@@ -29,21 +29,95 @@ exports.create = (req, res) => {
 };
 
 exports.findAll = (req, res) => {
-  
+  const name = req.query.name;
+  var condition = name ? { name: { [Op.iLike]: `%${name}%` } } : null;
+
+  Drink.findAll({ where: condition })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving drinks."
+      });
+    });
 };
 
 exports.findOne = (req, res) => {
-  
+  const id = req.params.id;
+
+  Drink.findByPk(id)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error retrieving Tutorial with id=" + id
+      });
+    });
 };
 
 exports.update = (req, res) => {
-  
+  const id = req.params.id;
+
+  Drink.update(req.body, {
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "Drink was updated successfully."
+        });
+      } else {
+        res.send({
+          message: `Cannot update drink with id=${id}. Maybe drink was not found or req.body is empty`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating drink with id=" + id
+      });
+    });
 };
 
 exports.delete = (req, res) => {
-  
+  const id = req.params.id;
+
+  Drink.destroy({
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "Drink deleted"
+        });
+      } else {
+        res.send({
+          message: `Cannot delete drink with id=${id}. Maybe drink was not found`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete drink with id=" + id
+      });
+    });
 };
 
 exports.deleteAll = (req, res) => {
-  
+  Drink.destroy({
+    where: {},
+    truncate: false
+  })
+    .then(nums => {
+      res.send({ message: `${nums} Drinks were deleted successfully` });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all drinks."
+      });
+    });
 };
